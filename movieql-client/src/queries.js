e7e9f3846ce6fc92dbf5255997a9dfbd5328444a
BearCola <sangin84@gmail.com>
2019-04-19 11:33:38 +0900

    Finish project

--- movieql-client/src/queries.js
+++ movieql-client/src/queries.js
@@ -5,8 +5,8 @@ export const HOME_PAGE = gql`
     movies(limit: 50, rating: 7) {
       id
       title
-      genres
       rating
+      medium_cover_image
     }
   }
 `;
@@ -22,9 +22,10 @@ export const MOVIE_DETAILS = gql`
       genres
     }
     suggestions(id: $movieId) {
-      medium_cover_image
+      id
       title
       rating
+      medium_cover_image
     }
   }
 `;
